8d6fddac7bc1ed9d0a7967d83e739d69ad74b592
Xlib <aman9893089064@gmail.com>
2024-05-11 22:49:20 +0530

    updated the experience link with the pixel streaming server

--- src/Fronend/Components/Models/Residential.tsx
+++ src/Fronend/Components/Models/Residential.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import '../Styles/Residential.css'; // Import the CSS file
 
 const Residential: React.FC = () => {
+  const hrefLink = 'https://ac14-2405-204-5215-ab0b-e5f2-e969-3851-b5dd.ngrok-free.app/';
+
   return (
     <div className="residential">
       <div className="filter-panel">
@@ -12,55 +14,55 @@ const Residential: React.FC = () => {
       <div className="property-cards">
         <div className="property-card">
           <p className=''>Property 1</p>
-          <button className='experience-button'>
+          <button className='experience-button' onClick={() => window.location.href = hrefLink}>
            Experience
           </button>
         </div>
         <div className="property-card">
           <p className=''>Property 1</p>
-          <button className='experience-button'>
+          <button className='experience-button' onClick={() => window.location.href = hrefLink}>
            Experience
           </button>
         </div>
         <div className="property-card">
           <p className=''>Property 1</p>
-          <button className='experience-button'>
+          <button className='experience-button' onClick={() => window.location.href = hrefLink}>
            Experience
           </button>
         </div>
         <div className="property-card">
           <p className=''>Property 1</p>
-          <button className='experience-button'>
+          <button className='experience-button' onClick={() => window.location.href = hrefLink}>
            Experience
           </button>
         </div>
         <div className="property-card">
           <p className=''>Property 1</p>
-          <button className='experience-button'>
+          <button className='experience-button' onClick={() => window.location.href = hrefLink}>
            Experience
           </button>
         </div>
         <div className="property-card">
           <p className=''>Property 1</p>
-          <button className='experience-button'>
+          <button className='experience-button' onClick={() => window.location.href = hrefLink}>
            Experience
           </button>
         </div>
         <div className="property-card">
           <p className=''>Property 1</p>
-          <button className='experience-button'>
+          <button className='experience-button' onClick={() => window.location.href = hrefLink}>
            Experience
           </button>
         </div>
         <div className="property-card">
           <p className=''>Property 1</p>
-          <button className='experience-button'>
+          <button className='experience-button' onClick={() => window.location.href = hrefLink}>
            Experience
           </button>
         </div>
         <div className="property-card">
           <p className=''>Property 1</p>
-          <button className='experience-button'>
+          <button className='experience-button' onClick={() => window.location.href = hrefLink}>
            Experience
           </button>
         </div>
