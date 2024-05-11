cc59dcbed5041ad7c86801d423e96151007433fd
Xlib <aman9893089064@gmail.com>
2024-05-11 21:36:31 +0530

    added files for various Classes

---
+++ src/Fronend/Components/Models/Residential.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import '../Styles/Residential.css'; // Import the CSS file
+
+const Residential: React.FC = () => {
+  return (
+    <div className="residential">
+      <div className="filter-panel">
+        <div>Buy</div>
+        <div>Rent</div>
+        <div>Price</div>
+      </div>
+      <div className="property-cards">
+        <div className="property-card">
+          <p className=''>Property 1</p>
+          <button className='experience-button'>
+           Experience
+          </button>
+        </div>
+        <div className="property-card">
+          <p className=''>Property 1</p>
+          <button className='experience-button'>
+           Experience
+          </button>
+        </div>
+        <div className="property-card">
+          <p className=''>Property 1</p>
+          <button className='experience-button'>
+           Experience
+          </button>
+        </div>
+        <div className="property-card">
+          <p className=''>Property 1</p>
+          <button className='experience-button'>
+           Experience
+          </button>
+        </div>
+        <div className="property-card">
+          <p className=''>Property 1</p>
+          <button className='experience-button'>
+           Experience
+          </button>
+        </div>
+        <div className="property-card">
+          <p className=''>Property 1</p>
+          <button className='experience-button'>
+           Experience
+          </button>
+        </div>
+        <div className="property-card">
+          <p className=''>Property 1</p>
+          <button className='experience-button'>
+           Experience
+          </button>
+        </div>
+        <div className="property-card">
+          <p className=''>Property 1</p>
+          <button className='experience-button'>
+           Experience
+          </button>
+        </div>
+        <div className="property-card">
+          <p className=''>Property 1</p>
+          <button className='experience-button'>
+           Experience
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Residential;
